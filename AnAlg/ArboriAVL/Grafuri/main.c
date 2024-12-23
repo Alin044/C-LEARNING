d00fd1d1e20e09948f7907ef59f79d9ae2a5971a
Alin <alinalin03030@gmail.com>
2024-12-23 22:22:06 +0000

    Update Grafuri

---
+++ AnAlg/ArboriAVL/Grafuri/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+int N[MAX];
+int A[MAX][MAX];
+int ctn = 0;
+
+void initializare(){
+    int i, j;
+    for(i = 0; i < MAX; i++){
+        for(j = 0; j < MAX; j++){
+            A[i][j] = 0;
+        }
+    }
+}
+
+void inserare_node(){
+    int x;
+    printf("\nIntroduceti valoarea unui nod : ");
+    scanf("%d", &x);
+    N[ctn] = x;
+    ctn++;
+}
+
+void insert_arc(){
+    int a, b;
+    do{
+        printf("\nInsert 2 noduri pentru a le asocia unui arc : ");
+        printf("\na : ");
+        scanf("%d", &a);
+        printf("\nb : ");
+        scanf("%d", &b);
+        A[a][b] = 1;
+        printf("\n1 to add | 0 to exit");
+        scanf("%d", &a);
+    }while(a);
+}
+
+void afisare(){
+    int i, j;
+    for(i = 0; i < ctn; i++){
+        printf("%d\t", N[i]);
+    }
+    printf("\n");
+    for(i = 0; i < ctn; i++){
+        for(j = 0; j < ctn; j++){
+            printf("%d\t", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+
+    return 0;
+}
